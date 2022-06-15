fadbb803d533cb0ef4884206a6c0697cd8773431
cstollen <cstollen@techfak>
2022-06-15 14:12:18 +0200

    Add WifiNINA firmware update script

---
+++ update-rp2040-wifinina-firmware.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [ ! -f "tools/arduino-fwuploader/arduino-fwuploader" ]; then
+	echo "Arduino firmware updater not found. Please run 'install-dependencies.sh'"
+	exit 1
+fi
+
+echo "Updating Arduino RP2040 Nano Connect WifiNINA module to firmware version 1.4.8"
+tools/arduino-fwuploader/arduino-fwuploader firmware flash -b arduino:mbed_nano:nanorp2040connect -a /dev/ttyACM0 -m NINA@1.4.8
+
