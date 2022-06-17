e96ae294fb799a19bd05b3c78abc7764a2710edf
cstollen <cstollen@techfak>
2022-06-17 17:07:20 +0200

    Fix 16 khz mic issue by setting sample frequency to 16.4 khz

--- rp2040-hotword-recognition-client/src/micro_features/micro_model_settings.h
+++ rp2040-hotword-recognition-client/src/micro_features/micro_model_settings.h
@@ -23,7 +23,8 @@ limitations under the License.
 // frequency information. This has to be a power of two, and since we're dealing
 // with 30ms of 16KHz inputs, which means 480 samples, this is the next value.
 constexpr int kMaxAudioSampleSize = 512;
-constexpr int kAudioSampleFrequency = 16000;
+// constexpr int kAudioSampleFrequency = 16000; // Sample frquency of the trained model
+constexpr int kAudioSampleFrequency = 16400; // On some rp2040 microcontrollers 16khz does not seem to work
 
 // The following values are derived from values used during model training.
 // If you change the way you preprocess the input, update all these constants.
